d8997dbb227d59065c770195017597054e1481c9
P.A.J.U.Pallewela <janithup8@gmail.com>
2025-04-28 09:41:11 +0530

    make Room occupency

--- room.c
+++ room.c
@@ -12,9 +12,9 @@ void viewAvailableRooms() {
     }
 
     Student s;
-    int roomCounts[NUMBER_OF_ROOMS] = {0}; // array to store number of students in each room
+    int roomCounts[NUMBER_OF_ROOMS] = {0};
 
-    // Read all students and count
+    
     while (fread(&s, sizeof(Student), 1, fp)) {
         if (s.room >= 0 && s.room < NUMBER_OF_ROOMS) {
             roomCounts[s.room]++;
@@ -24,32 +24,48 @@ void viewAvailableRooms() {
 
     printf("\n--- Available Rooms ---\n");
     for (int i = 0; i < NUMBER_OF_ROOMS; i++) {
-        if (roomCounts[i] < 4) { // room is available
+        if (roomCounts[i] < 4) { 
             printf("Room %d - %d spaces available\n", i, 4 - roomCounts[i]);
         }
     }
 }
 
 void checkRoomOccupancy() {
-    FILE *fp = fopen("students.dat", "r");
+    FILE *fp = fopen("students.dat", "rb"); // binary read
     if (fp == NULL) {
         printf("Error opening file.\n");
         return;
     }
 
-    Student s;
-    int roomCounts[NUMBER_OF_ROOMS] = {0};
+    Student students[1000]; 
+    int studentCount = 0;
 
-    // Read all students and count
-    while (fread(&s, sizeof(Student), 1, fp)) {
-        if (s.room >= 0 && s.room < NUMBER_OF_ROOMS) {
-            roomCounts[s.room]++;
-        }
+    
+    while (fread(&students[studentCount], sizeof(Student), 1, fp)) {
+        studentCount++;
     }
     fclose(fp);
 
     printf("\n--- Room Occupancy ---\n");
-    for (int i = 0; i < NUMBER_OF_ROOMS; i++) {
-        printf("Room %d - %d students\n", i, roomCounts[i]);
+
+    for (int room = 0; room < NUMBER_OF_ROOMS; room++) {
+        int roomStudentCount = 0;
+        
+        
+        for (int i = 0; i < studentCount; i++) {
+            if (students[i].room == room) {
+                if (roomStudentCount == 0) {
+                    printf("\nRoom %d:\n", room);
+                }
+                printf("  TgNo: %d, Name: %s\n", students[i].tgNo, students[i].name);
+                roomStudentCount++;
+            }
+        }
+        
+        if (roomStudentCount == 0) {
+            // Room is empty
+            // Optional: Uncomment below if you want to print empty rooms
+            // printf("\nRoom %d: Empty\n", room);
+        }
     }
 }
